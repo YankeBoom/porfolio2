1abf048fbd1ab35ec52c81ca809c1ddecba89846
YankeBoom <118504782+YankeBoom@users.noreply.github.com>
2024-02-16 18:17:10 +0700

    Add files via upload

---
+++ admin_statement.php
@@ -0,0 +1,63 @@
+<?php
+	echo "<link rel='stylesheet' href=css/style.css>";
+{
+		try {
+			$conn = new PDO('mysql:host=localhost;dbname=student_MaAleksV20','MaAleksV20','srCieTna');
+
+
+			$query = 'SELECT * FROM `statement` WHERE `id`';
+
+			$result = $conn->query($query);
+
+			echo '<h2 class="section_title">Панель администратора';
+
+			echo '<table class="form__input" border="1">';
+				foreach ($result as $data) {
+					try{
+				$conn = new PDO('mysql:host=localhost;dbname=student_MaAleksV20','MaAleksV20','srCieTna');
+
+						$query = 'SELECT * FROM `statement` WHERE `id`';
+
+						$gruppa = $conn->query($query);
+
+						$conn = NULL;
+					} catch (PDOException $e) {
+						print 'ERROR> '.$e->getMessage();
+						die();	
+					}
+                    
+                    echo '</td></tr>
+                    <tr><td><h2>Заявление №'.$data['id'].'<br>
+										<form action="admin_formulate_statement.php" method="post" class="form form-send">
+                    <input type="text" name="statement" value="'.$data['confirmation'].'">
+                    <input type="submit" name="confirmation" value="Заявка подтверждена ✔">
+										<input type="submit" name="confirmation" value="Заявка отклонена ×">
+										</form>
+                    <tr><td>Регистрацоный номер автомобиля: '.$data['car_number'].'</td></tr>
+                    <td>Описание нарушения: '.$data['description'].'<br></br></td>';
+                }
+
+			echo '</table></h2>';
+			echo '<a class="btn form__btn" href="index.html">Выход</a>';
+
+			$conn = NULL;
+		} catch(PDOException $e) {
+			print 'ERROR: >' .$e->getMessage();
+			die();
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+
+
+  <title>Нарушениям.Нет</title>
+</head>
+<body>
+</body>
+</html>
